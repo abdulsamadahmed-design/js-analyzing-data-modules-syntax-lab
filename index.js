2d1e7a11ac26af5e357d7f5d4a43dc231a745c68
Abdulsamad Ahmed <Abdulsamad.ahmed@student.moringaschool.com>
2026-07-05 15:22:21 +0300

    hello

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 
+function combineUsers(...arrays) {
+  const users = [].concat(...arrays);
+  const today = new Date();
+
+const merge_date =
+  `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
+
+  return {
+    users,
+    merge_date
+  };
+}
+
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
